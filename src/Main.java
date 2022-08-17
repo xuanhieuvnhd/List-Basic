da45c066e6646ed9e8a1db91cbd2116c1417b557
xuanhieuvnhd <xuanhieuvn@gmail.com>
2022-08-17 11:46:15 +0700

    17/8

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        MyList<Integer> listInteger = new MyList<Integer>();
+        listInteger.add(1);
+        listInteger.add(2);
+        listInteger.add(3);
+
+        System.out.println(listInteger);
+    }
+}
